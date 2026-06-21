85c13a6bec03bfb9dfa1572b937de51b715af0b5
muhammadsyauqijazuli <syauqialjm93@gmail.com>
2026-06-21 17:37:59 +0700

    refactor: remove deprecated database connection utility module

--- backend/update.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE students ADD COLUMN jurusan ENUM('TKJ','TAV','TPTU','MULTIMEDIA') DEFAULT NULL;
-ALTER TABLE predictions MODIFY risk_status ENUM('Sangat Beresiko','Beresiko','Aman') NOT NULL;
-ALTER TABLE predictions ALTER COLUMN model_version SET DEFAULT '3.0.0';
-ALTER TABLE shap_analysis MODIFY feature_name VARCHAR(100) NOT NULL;
